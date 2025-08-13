af818d71a5fbf89931da29bdb5cdfe1df344a508
Swati228 <78743406+Swati228@users.noreply.github.com>
2025-08-13 11:54:39 +0530

    Update the node version to 22.x

--- publish.sh
+++ publish.sh
@@ -79,8 +79,8 @@ if ! [ -x "$(command -v npm)" ]; then
 echo 'Error: npm is not installed and required.' >&2
 exit 1
 fi
-if ! node -v | grep -qF "v18."; then
-  echo 'Error: Node.js version 18.x is not installed and required.' >&2
+if ! node -v | grep -qF "v22."; then
+  echo 'Error: Node.js version 22.x is not installed and required.' >&2
   exit 1
 fi
 
